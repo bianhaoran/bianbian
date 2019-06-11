b8361e760f200c41794e90734d98bbbec0b8927c
bianhaoran <bianhaoran>
2019-06-11 17:01:10 +0800

    000

---
+++ GitTest/src/cn/itcast/git/gitTest.java
@@ -0,0 +1,10 @@
+package cn.itcast.git;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("你过来啊");
+        System.out.println("你过来啊");
+        System.out.println("你过来啊");
+        System.out.println("你过来啊");
+    }
+}
